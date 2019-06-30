392b7de27455d5f43d33f33fb3be75ff4fb5c735
antuor <antourv@gmail.com>
2019-06-30 21:57:47 +0700

    Rectangle

--- IsometricMonoGame.core/GameCore.cs
+++ IsometricMonoGame.core/GameCore.cs
@@ -4,13 +4,11 @@ using Microsoft.Xna.Framework.Input;
 
 namespace IsometricMonoGame
 {
-    /// <summary>
-    /// This is the main type for your game.
-    /// </summary>
     public class GameCore : Game
     {
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
+        Texture2D sprite;
         
         public GameCore()
         {
@@ -37,10 +35,8 @@ namespace IsometricMonoGame
         /// </summary>
         protected override void LoadContent()
         {
-            // Create a new SpriteBatch, which can be used to draw textures.
             spriteBatch = new SpriteBatch(GraphicsDevice);
-
-            // TODO: use this.Content to load your game content here
+            sprite = Content.Load<Texture2D>(@"Sprites/Rectangle");
         }
 
         /// <summary>
@@ -74,9 +70,9 @@ namespace IsometricMonoGame
         protected override void Draw(GameTime gameTime)
         {
             GraphicsDevice.Clear(Color.CornflowerBlue);
-
-            // TODO: Add your drawing code here
-
+            spriteBatch.Begin();
+            spriteBatch.Draw(sprite, new Vector2(0, 0), Color.White);
+            spriteBatch.End();
             base.Draw(gameTime);
         }
     }
